1b3995f7f4e01f575dcb4fef3d79a5e004410bac
AdvikGarg <gargadvik23@gmail.com>
2025-11-07 17:22:01 +0530

    Add files via upload

---
+++ Birthadayha.py
@@ -0,0 +1,12 @@
+#creating variables
+Bday1 = ("23 August")
+
+Bday2 = ("22 March")
+
+Bday3 = ("3 October")
+
+Bday4 = ("9 September")
+
+Bday5 = ("7 July")
+ 
+print("The 5 Bday of Raj's friend are:", Bday1, Bday2, Bday2, Bday3, Bday4, Bday5)
