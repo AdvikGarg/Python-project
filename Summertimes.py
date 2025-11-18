4079dfeefa085e351c19b1e6d0c8fbfdb1832ec5
AdvikGarg <gargadvik23@gmail.com>
2025-11-18 16:39:28 +0530

    Add files via upload

---
+++ Summertimes.py
@@ -0,0 +1,11 @@
+sweatshirt="light, weather appropiate"
+sweater="light, weather appropiate"
+
+clothes=str(input("Rohan should wear: "))
+
+if ("answer is", sweatshirt, sweater):
+    print("YOU CAN WEAR IT!")
+    
+else:
+    print("YOU CANNOT WEAR IT!")    
+ 
