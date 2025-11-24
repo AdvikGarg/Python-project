0d81fc0cfd8e612819eaa8023cb7ac9252b6248e
AdvikGarg <gargadvik23@gmail.com>
2025-11-24 21:33:04 +0530

    Update Alphabet.py

--- Alphabet.py
+++ Alphabet.py
@@ -1,9 +1,10 @@
 enter=(input("Enter any number or aphabet: "))
-if(enter==str):
-    
-   print("Its an alphabet ")
-    
+if enter>="a" and enter<='z' or enter>='A' and enter<='Z' :
+   print("It's an alphabet")
+elif enter>='0' and enter<='9' : 
+   print("Its a digit") 
 else:
-    print("Its not a alphabet")   
+   print("Something else ")   
   
+
  
