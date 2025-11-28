17be560d9d36dbd7f784f0274a0eec32885b9358
AdvikGarg <gargadvik23@gmail.com>
2025-11-28 16:55:17 +0530

    Add files via upload

---
+++ swap .py
@@ -0,0 +1,20 @@
+a=float(input('Enter a '))
+b=float(input('Enter b '))
+c=float(input('Enter c '))
+e=float(input('Enter e '))
+d=float(input('Enter d '))
+f=float(input('Enter f '))
+
+print('a=c')
+print('b=d')
+print('e=f')
+
+a=c
+b=d
+e=f
+
+print('The value of a after swapping is:,', a)
+print('The value of b after swapping is:,', b)
+print('The value of c after swapping is:,', c)
+
+
