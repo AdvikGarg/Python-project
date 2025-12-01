11dbed0d53d9d1a733dd03119599a668792d316f
AdvikGarg <gargadvik23@gmail.com>
2025-12-01 22:04:14 +0530

    Update swap .py

--- swap .py
+++ swap .py
@@ -1,20 +1,15 @@
 a=float(input('Enter a '))
 b=float(input('Enter b '))
 c=float(input('Enter c '))
-e=float(input('Enter e '))
-d=float(input('Enter d '))
-f=float(input('Enter f '))
 
-print('a=c')
-print('b=d')
-print('e=f')
-
+d=a
 a=c
+c=b
 b=d
-e=f
 
 print('The value of a after swapping is:,', a)
 print('The value of b after swapping is:,', b)
 print('The value of c after swapping is:,', c)
+
 
 
