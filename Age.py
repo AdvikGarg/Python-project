192e9778d4d5ae2f5ba46e109d5cca5adef3ea93
AdvikGarg <gargadvik23@gmail.com>
2025-12-03 10:18:01 +0530

    Add files via upload

---
+++ Age.py
@@ -0,0 +1,12 @@
+age=int(input('Enter your age: '))
+        
+if age>=10 and age<=20:
+   print('You are allowed!')
+else:
+    if age<10 or age>20:
+        print('You are not allowed!') 
+        
+       
+         
+                                
+            
